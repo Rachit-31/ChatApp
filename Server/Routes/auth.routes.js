085d8c975eaab07e43ff703698022bff36f5f8d9
Rachit Jain <rachitjainjaipur@gmail.com>
2024-05-28 22:03:13 +0530

    code needs to be revised as not working

--- Server/Routes/auth.routes.js
+++ Server/Routes/auth.routes.js
@@ -2,7 +2,16 @@ import express from "express";
 const router = express.Router();
 import { signup,logout,login  } from "../controller/auth.Controller.js";
 
-router.post("/signup",signup);
-router.post("/login",login);
-router.post("/logout",logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
 export default router;
+
+
+
+
+
+
+
+
--- Server/server.js
+++ Server/server.js
@@ -3,19 +3,33 @@
 // to run backend server type npm run server
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from 'body-parser';
 
-import authRoutes from "../Server/Routes/auth.routes.js"
+
+import authRoutes from "./Routes/auth.routes.js"
 import connectToMongoDb from "./db/connectToMongoDb.js";
 
 const app=express();
 const PORT=process.env.PORT || 5000;
 
 dotenv.config();
-app.use("/api/auth",authRoutes);
+
+
+
 app.use(express.json()); //to parse the incomming request with json payloads(from req.body)
+app.use(express.urlencoded({ extended: true }));
 
 
 
+// app.use("/api/auth",authRoutes);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// // Logging middleware
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.url}`);
+//     next();
+// });
 
 // app.get("/",(req,res)=>{
 //     // root route
