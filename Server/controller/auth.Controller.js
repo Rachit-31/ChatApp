a877df7beff7826aca22e4fe7de620d10fe59561
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-13 10:51:58 +0530

    logout and login controller done

--- Server/controller/auth.Controller.js
+++ Server/controller/auth.Controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcrypt';
+import generateTokenAandSetCookie from '../utils/generateToken.js';
 
 export const signup = async (req, res) => {
 	try {
@@ -34,6 +35,7 @@ export const signup = async (req, res) => {
 
 		if (newUser) {
 			// Generate JWT token here
+			generateTokenAandSetCookie(newUser._id, res);
 			await newUser.save();
 
 			res.status(201).json({
@@ -53,9 +55,47 @@ export const signup = async (req, res) => {
 };
 
 
-export const login=(req,res)=>{
-    console.log("login user");
-}
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        console.log(req.body);
+        if (!username || !password) {
+            return res.status(400).json({ error: "Username or password required" });
+        }
+
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(400).json({ error: "Invalid username or password" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ error: "Invalid username or password" });
+        }
+
+        generateTokenAandSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
 export const logout=(req,res)=>{
-    console.log("logout user");
+    try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 }
--- Server/utils/generateToken.js
+++ Server/utils/generateToken.js
@@ -2,5 +2,13 @@ import jwt from 'jsonwebtoken'
 const generateTokenAandSetCookie = (userId, res)=>{
     const token = jwt.sign({userId} , process.env.JWT_SECRET, {
         expiresIn:process.env.JWT_EXPIERY
+    });
+
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000,
+        httpOnly:true,  //prevent xss attacks
+        sameSite:"strict",
+        secure: process.env.NODE_ENV !== "development",
     })
 }
+export default generateTokenAandSetCookie;
