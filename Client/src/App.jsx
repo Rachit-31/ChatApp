5038a6f2090e756d28a0751d25fb6fe626579861
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-18 08:38:53 +0530

    tailwind and daisy ui set up completed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,8 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  
 
   return (
     <>
-    <h1>chatapp</h1>
+    <button className="btn btn-primary">Primary</button>
     </>
   )
 }
