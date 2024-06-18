725cef3eff6888e3165cb4f6a337cb7ec4916dfa
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-18 07:20:34 +0530

    user controller done

--- Server/server.js
+++ Server/server.js
@@ -21,10 +21,13 @@ const PORT = process.env.PORT || 5000;
  app.use(express.urlencoded({ extended: true }));
  app.use(cookieParser());   
 
+
 import authRoutes from "./Routes/auth.routes.js";
 import messageRoutes from "./Routes/message.routes.js"
+import userRoutes from "./Routes/user.routes.js"
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
 
 
