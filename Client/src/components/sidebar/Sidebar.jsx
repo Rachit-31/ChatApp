870bcc41202ea2e65a4c60595b772cf06b7cb586
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-19 14:37:46 +0530

    file created convesation

---
+++ Client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import SearchInput from "./SearchInput"
+import Conversations from "./Conversations"
+const Sidebar = () => {
+  return (
+    <div>
+    <SearchInput/>
+    <div className="divider px-3"></div>
+    {/*  conversation */}
+     {/* logout button */}
+    </div>
+  )
+}
+
+export default Sidebar
