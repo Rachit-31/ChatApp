592dd35c0b9951ad07c3ed85bcf1cb66923d7636
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-20 08:57:50 +0530

    .

--- Client/src/pages/signup/SignUp.jsx
+++ Client/src/pages/signup/SignUp.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import GenderCheckbox from "./GenderCheckbox"
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+  const [inputs, setInputs]= useState({
+    fullName: '',
+    username:'',
+    password:'',
+    confirmPassword:'',
+    gender:''
+  })
+  
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(inputs)
+  }
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -9,14 +22,15 @@ const SignUp = () => {
 					Sign Up <span className='text-red-700'> CommuneChat</span>
 				</h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
             <span className='text-base label-text'>Full Name</span>
             </label>
             <input type='text'
 							placeholder='Rachit Jain'
-							className='w-full input input-bordered  h-10' />
+							className='w-full input input-bordered  h-10' value={inputs.fullName}
+              onChange ={ ( e ) => setInputs({...inputs, fullName: e.target.value})} />
           </div>
 
           <div>
@@ -25,7 +39,9 @@ const SignUp = () => {
             </label>
             <input type='text'
 							placeholder='rachitj'
-							className='w-full input input-bordered h-10' />
+							className='w-full input input-bordered h-10'
+              value={inputs.username}
+							onChange={(e) => setInputs({ ...inputs, username: e.target.value })} />
           </div>
 
           <div>
@@ -34,7 +50,10 @@ const SignUp = () => {
             </label>
             <input type='password'
 							placeholder='Enter Password'
-							className='w-full input input-bordered h-10' />
+							className='w-full input input-bordered h-10'
+              value={inputs.password}
+              onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+              />
           </div>
 
           <div>
