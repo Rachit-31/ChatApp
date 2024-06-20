c862b851c900d3989c16e1a76581ee7e20d1976a
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-20 07:12:56 +0530

    react router dom completed

--- Client/src/pages/signup/SignUp.jsx
+++ Client/src/pages/signup/SignUp.jsx
@@ -1,4 +1,6 @@
 import GenderCheckbox from "./GenderCheckbox"
+import { Link } from 'react-router-dom';
+
 const SignUp = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
@@ -46,7 +48,7 @@ const SignUp = () => {
 
           <GenderCheckbox/>
 
-          <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">Already have an account?</a>
+          <Link to="/login" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">Already have an account?</Link>
           <div>
             <button className="btn btn-block btn-sm mt-2 border border-slate-700">Sign Up</button>
           </div>
