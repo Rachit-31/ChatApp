b47ca84f80715dc58acd0ee4cfd60a7e99608431
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-30 14:10:51 +0530

    fetched conversation from backend

--- Client/src/components/sidebar/Conversations.jsx
+++ Client/src/components/sidebar/Conversations.jsx
@@ -1,19 +1,23 @@
 import Conversation from "./Conversation"
 import useGetConversations from "../../hooks/useGetConversations"
+import { getRandomEmoji } from "../../utils/emojis";
 const Conversations = () => {
-  const {loading, conversations}= useGetConversations();
+  const { loading, conversations } = useGetConversations();
   console.log("Conversations: ", conversations)
   return (
-    <div className="py-2 flex flex-col overflow-auto">
-      {conversations.map((conversation, idx) => {
-        <Conversation
-            key={conversation.idx}
-            conversation={conversation}
-            emoji={getRandomEmoji()}
-            lastIdx={idx === conversations.length - 1} />
-      })}
-        {loading ? <span className="loading loading-spinner mx-auto"></span> : null}
-    </div>
+
+    <div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
+
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
   )
 }
 
