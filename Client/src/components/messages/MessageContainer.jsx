a99b34ebde002b28023f0d33a14016c9bb4ad7a8
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-30 15:31:18 +0530

    made message container dynamic

--- Client/src/components/messages/MessageContainer.jsx
+++ Client/src/components/messages/MessageContainer.jsx
@@ -1,18 +1,34 @@
 import Messages from "./Messages"
 import MessageInput from "./MessageInput"
 import { TiMessages } from "react-icons/ti";
+import useConversation from "../../zustand/useConversation";
+import { useEffect } from "react";
 const MessageContainer = () => {
-  return (
-    <div className="md:min-w-[450px] flex flex-col">
-        <>
-        <div className="bg-slate-500 px-4 py-2 mb-2">
-            <span className="label-text">To:</span> <span className="text-gray-900 font-bold">Rachit Jain</span>
-        </div>
-        </>
-        <Messages/>
-        <MessageInput/>
-    </div>
-  )
+    const { selectedConversation, setSelectedConversation } = useConversation();
+
+    // to remove the selected component when browser is closed
+    useEffect(() => {
+        // clean up
+        return() => setSelectedConversation(null)
+    },[setSelectedConversation])
+
+	return (
+		<div className='md:min-w-[450px] flex flex-col'>
+			{!selectedConversation ? (
+				<NoChatSeleted />
+			) : (
+				<>
+					{/* Header */}
+					<div className='bg-slate-500 px-4 py-2 mb-2'>
+						<span className='label-text'>To:</span>{" "}
+						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+					</div>
+					<Messages />
+					<MessageInput />
+				</>
+			)}
+		</div>
+	);
 }
 
 export default MessageContainer
