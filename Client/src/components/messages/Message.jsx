54bcaf6bfcf24b60c579fe3ac2bef0bb0ecd454f
Rachit Jain <rachitjainjaipur@gmail.com>
2024-07-06 19:02:34 +0530

    half work done

--- Client/src/components/messages/Message.jsx
+++ Client/src/components/messages/Message.jsx
@@ -1,5 +1,17 @@
+import { useAuthContext } from "../../context/AuthContext"
+import useConversation from "../../zustand/useConversation";
 
-const Message = () => {
+const Message = ({message}) => {
+  const {authUser} = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id
+  const formattedTime =  extractTime(message.createdAt);
+  const chatClassName = fromMe? "chat-end" : "chat-start";
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+  const shakeClass = message.shouldShake ? "shake" : "";
+  
   return (
     <div className="chat chat-end">
         <div className="chat-image avatar">
--- Client/src/components/messages/Messages.jsx
+++ Client/src/components/messages/Messages.jsx
@@ -1,15 +1,24 @@
 import useGetMessages from "../../hooks/useGetMessages"
 import Message from "./Message"
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 const Messages = () => {
   const { messages, loading } = useGetMessages();
-  //console.log(messages)
+  console.log(messages)
   return (
     <div className="px-4 flex-1 overflow-auto">
-        <Message/>
+      {!loading &&
+          messages.length > 0 &&
+          messages.map((message) => (
+					<div key={message._id} >
+						<Message message={message} />
+					</div>
+				))}
+        {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+        {!loading && messages.length === 0 && (
+				<p className='text-center'>Send a message to start the conversation</p>
+			)}
     </div>
   )
 }
 
 export default Messages
-
-//3:39:28 continue from now
